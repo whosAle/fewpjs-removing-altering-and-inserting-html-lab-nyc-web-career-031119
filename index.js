c813dc2604ef3a748976092ac5a1ff7045553dc7
whosAle <abelgrave95@gmail.com>
2019-04-25 13:41:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.querySelector('#main');
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerText = "YOUR-NAME is the champion";
+document.querySelector('body').appendChild(newHeader);
